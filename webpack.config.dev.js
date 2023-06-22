22fe4ba4bc847c0b3749e3901d21c403b6b9f3d4
bglamadrid <bg.lamadrid@gmail.com>
2023-06-22 15:24:11 -0400

    style: simpler config object construction

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,41 +2,39 @@ const path = require('path');
 const PugPlugin = require('pug-plugin');
 const baseConfig = require('./webpack.config.base');
 
-module.exports = Object.assign(
-  baseConfig,
-  {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    output: {
-      filename: 'scripts/[name].[contenthash:8].js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      clean: true
-    },
-    plugins: [
-      new PugPlugin({
-        extractCss: {
-          filename: 'styles/[name].[contenthash:8].css'
-        }
-      })
-    ],
-    module: {
-      rules: [
-        ...baseConfig.module.rules,
-        {
-          test: /\.pug$/,
-          loader: PugPlugin.loader,
-          options: {
-            esModule: true,
-            data: {
-              environment: require('./src/environment.js')
-            }
+module.exports = {
+  ...baseConfig,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'scripts/[name].[contenthash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true
+  },
+  plugins: [
+    new PugPlugin({
+      extractCss: {
+        filename: 'styles/[name].[contenthash:8].css'
+      }
+    })
+  ],
+  module: {
+    rules: [
+      ...baseConfig.module.rules,
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader,
+        options: {
+          esModule: true,
+          data: {
+            environment: require('./src/environment.js')
           }
         }
-      ]
-    }
+      }
+    ]
   }
-);
+};
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,46 +3,44 @@ const PugPlugin = require('pug-plugin');
 const baseConfig = require('./webpack.config.base');
 const { NormalModuleReplacementPlugin } = require('webpack');
 
-module.exports = Object.assign(
-  baseConfig,
-  {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-    },
-    output: {
-      filename: 'scripts/[name].[contenthash:8].js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      clean: true
-    },
-    plugins: [
-      new PugPlugin({
-        extractCss: {
-          filename: 'styles/[name].[contenthash:8].css'
-        }
-      }),
-      // for client-side javascript
-      new NormalModuleReplacementPlugin(
-        /\.\.\/environment/,
-        '../environment.prod.js'
-      )
-    ],
-    module: {
-      rules: [
-        ...baseConfig.module.rules,
-        {
-          test: /\.pug$/,
-          loader: PugPlugin.loader,
-          options: {
-            esModule: true,
-            data: {
-              environment: require('./src/environment.prod.js') // for pug templates
-            }
+module.exports = {
+  ...baseConfig,
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: 'scripts/[name].[contenthash:8].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    clean: true
+  },
+  plugins: [
+    new PugPlugin({
+      extractCss: {
+        filename: 'styles/[name].[contenthash:8].css'
+      }
+    }),
+    // for client-side javascript
+    new NormalModuleReplacementPlugin(
+      /\.\.\/environment/,
+      '../environment.prod.js'
+    )
+  ],
+  module: {
+    rules: [
+      ...baseConfig.module.rules,
+      {
+        test: /\.pug$/,
+        loader: PugPlugin.loader,
+        options: {
+          esModule: true,
+          data: {
+            environment: require('./src/environment.prod.js') // for pug templates
           }
         }
-      ]
-    }
+      }
+    ]
   }
-);
+};
